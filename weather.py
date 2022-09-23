2192cd2e1caa1bdc6c3a81983decad2a39ea5c60
Datta Kiran Rao <dattakiranrao@192.168.1.2>
2022-09-23 18:10:12 +0530

    first commit

---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+
+API_KEY = 'a8e20995610bceaa81374001e509b5c4'
+BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+city = input('Enter a city name: ')
+
+request_url = f'{BASE_URL}?appid={API_KEY}&q={city}'
+
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    # print(data)
+    name = data['name']
+    print(name)
+    weather = data['weather'][0]['description']
+    print(weather)
+    temperature = data['main']['temp'] - 273.15
+    print(f"{round(temperature, 2)}`C")
+    country = data['sys']['country']
+    print(country)
+else:
+    print("an error occurred.")
+
